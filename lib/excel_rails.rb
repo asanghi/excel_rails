08f7ac07d9a949f70af0563619ebf01c81a8022e
Roman Simecek <roman.simecek@cyt.ch>
2012-01-27 09:51:58 +0100

    Add an option for a own renderer of the workbook.

--- lib/excel_rails.rb
+++ lib/excel_rails.rb
@@ -8,7 +8,7 @@ module Spreadsheet
         download = opts.delete(:force_download)
         filename = opts.delete(:filename)
         template_path = opts.delete(:template_path)
-        workbook = template_path ? Spreadsheet.open(template_path) : Spreadsheet::Workbook.new
+        workbook = template_path ? Spreadsheet.open(template_path) : (opts.delete(:renderer) || Spreadsheet::Workbook).new
         yield(workbook)
         disposition(download, filename) if (download || filename)
         workbook
